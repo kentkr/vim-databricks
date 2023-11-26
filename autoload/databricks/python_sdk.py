e6340f4a5dd059dbbf1b842ef54fb98bd57c81cb
Kyle Kent <kyle.kent321@gmail.com>
2023-11-26 11:48:25 -0600

    added untested databricks execution call

--- autoload/databricks/python_sdk.py
+++ autoload/databricks/python_sdk.py
@@ -1,11 +1,21 @@
 
 import argparse
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.service import compute
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--code')
     args = parser.parse_args()
 
+    client = WorkspaceClient()
+    cluster_id = ''
+    context = client.command_execution.create(cluster_id=cluster_id, language=compute.Language.PYTHON).result()
+    results = client.command_execution.create(cluster_id=cluster_id,
+                                              context_id=context.id,
+                                              language=compute.Language.PYTHON,
+                                              command = args.code)
+    client.command_execution.destroy(cluster_id=cluster_id, context_id=context.id)
     print(args.code)
 
 if __name__ == "__main__":
