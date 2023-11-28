f5209f1bc999e619e888e17aca5dd5956a424475
Kyle Kent <kyle.kent321@gmail.com>
2023-11-27 18:18:18 -0600

    able to execute code on databricks now

--- autoload/databricks/python_sdk.py
+++ autoload/databricks/python_sdk.py
@@ -3,20 +3,26 @@ import argparse
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service import compute
 
+def execute_code(cmd, profile, cluster_id) -> str:
+    client = WorkspaceClient(profile=profile)
+    #cluster_id = '0420-160411-p8oi50n1'
+    context = client.command_execution.create(cluster_id=cluster_id, language=compute.Language.python).result()
+    response = client.command_execution.execute(cluster_id=cluster_id,
+                                                context_id=context.id,
+                                                language=compute.Language.python,
+                                                command=cmd).result()
+    client.command_execution.destroy(cluster_id=cluster_id, context_id=context.id)
+
+    return response.results.data
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--code')
+    parser.add_argument('--profile')
+    parser.add_argument('--cluster_id')
     args = parser.parse_args()
 
-    client = WorkspaceClient()
-    cluster_id = ''
-    context = client.command_execution.create(cluster_id=cluster_id, language=compute.Language.PYTHON).result()
-    results = client.command_execution.create(cluster_id=cluster_id,
-                                              context_id=context.id,
-                                              language=compute.Language.PYTHON,
-                                              command = args.code)
-    client.command_execution.destroy(cluster_id=cluster_id, context_id=context.id)
-    print(args.code)
+    print(execute_code(args.code, args.profile, args.cluster_id))
 
 if __name__ == "__main__":
     main()
