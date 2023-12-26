d6bfaf71862c5069139918ef56ebab1240f32555
Kyle Kent <kyle.kent321@gmail.com>
2023-12-26 01:32:06 -0600

    finally tests

--- tests/pytest/test_python_sdk.py
+++ tests/pytest/test_python_sdk.py
@@ -1,50 +1,32 @@
 
 import os
 import sys
-
 # put plugin dir in path for importing
 plugin_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 sys.path.insert(0, plugin_dir)
+from unittest.mock import MagicMock, patch
+from autoload.databricks.python_sdk import context_is_running
 
-from autoload.databricks.python_sdk import execute_code
-import pytest
-from unittest.mock import MagicMock
 
-class MockCommandExecution:
-    def create(self, cluster_id, language) -> MagicMock:
-        result = MagicMock()
-        result.id = 'context_id'
-        return result
+def test_context_running():
+    with patch('autoload.databricks.python_sdk.WorkspaceClient') as MockedClient:
+        mock_command_execution=MagicMock()
+        mock_command_execution.command_execution.context_status().status.value='Running'
+        MockedClient.return_value=mock_command_execution
+        assert context_is_running('','','')
 
-    def execute(self, cluster_id, context_id, language, command):
-        self.result = 'Mock command output'
-        return self
+def test_context_not_running():
+    with patch('autoload.databricks.python_sdk.WorkspaceClient') as MockedClient:
+        mock_command_execution=MagicMock()
+        mock_command_execution.command_execution.context_status().status.value='Something else'
+        MockedClient.return_value=mock_command_execution
+        assert not context_is_running('','','')
 
-    def destroy(self, cluster_id, context_id) -> None:
-        pass
+def test_context_exception():
+    with patch('autoload.databricks.python_sdk.WorkspaceClient') as MockedClient:
+        mock_command_execution=MagicMock()
+        mock_command_execution.command_execution.context_status().side_effect=Exception
+        MockedClient.return_value=mock_command_execution
+        assert not context_is_running('','','')
 
-    def result(self):
-        return self.result
-
-class MockClient:
-    def __init__(self, profile) -> None:
-        self.profile = profile
-        self.command_execution = MockCommandExecution()
-        pass
-
-@pytest.fixture
-def mock_client(monkeypatch):
-    def mock_client_class(*args, **kwargs):
-        return MockClient(*args, **kwargs)
-    
-    monkeypatch.setattr("autoload.databricks.python_sdk.WorkspaceClient", mock_client_class)
-
-def test_execute_code(mock_client):
-    code = 'print("hello")'
-    profile = 'mock_profile'
-    cluster_id = 'mock_cluster_id'
-
-    output = execute_code(code, profile, cluster_id)
-
-    assert output == 'Mock command output'
 
